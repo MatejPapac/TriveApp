e82b5c56b132c8c59a1d29a8ef05d3fd6dcbaa4a
MatejPapac <papac.tmrip@gmail.com>
2022-05-03 10:30:35 +0200

    Zavrsio meni za skladište

--- Program.php
+++ Program.php
@@ -2,6 +2,7 @@
 class Program
 {
 private $podaci;
+private $kosarica;
 public function __construct()
 {
     $this->podaci=[];
@@ -15,14 +16,13 @@ private function izbornik()
 
     echo '1.Pregled produkta' . PHP_EOL;
     echo '2.Unos produkta' . PHP_EOL;
-    echo '3.Brisanje Produkta' . PHP_EOL;
-    echo '4.Ulazak u košaricu' . PHP_EOL;
-    echo '5.Izlazak iz programa' .PHP_EOL;
+    echo '3.Ulazak u košaricu' . PHP_EOL;
+    echo '4.Izlazak iz programa' .PHP_EOL;
 
     $izbor=0;
     while(true) {
         $izbor= Controller::ucitajInt('Izaberi od ponuđenih opcija: ','Nisi unio cijeli broj');
-        if($izbor  <1 || $izbor >5){
+        if($izbor  <1 || $izbor >4){
             echo 'Moras uzeti od ponuđenih odabira' .PHP_EOL;
             continue;
         }
@@ -39,12 +39,9 @@ private function izbornik()
             $this->unosProdukta();
             break;
          case 3:
-            $this->brisanjeProdukta();
-            break;
-         case 4:
             $this->kosaricaMenu();
             break;
-            case 5:
+            case 4:
            echo 'Doviđenja' ;
             
         }     
@@ -61,9 +58,88 @@ private function izbornik()
         $this->izbornik();
 
     }
+
+    private function pregledProdukta()
+        {
+            foreach ($this->podaci as $p) {
+                echo $p->getIme() . ' ' . $p->getCijena() . ' ' . $p->getSifra() . PHP_EOL;
+            }
+        $this->izbornik();
+        }
+
+
+
+        private function kosaricaMenu()
+
+        {
+            echo '1.Dodaj u košaricu' .PHP_EOL;
+            echo '2.Izbriši iz košarice' .PHP_EOL;
+            echo '3.Plati' .PHP_EOL;
+            echo '4.Izlaz' .PHP_EOL;
+
+
+
+
+            $izbor=0;
+    while(true) {
+        $izbor= Controller::ucitajInt('Izaberi od ponuđenih opcija: ','Nisi unio cijeli broj');
+        if($izbor  <1 || $izbor >4){
+            echo 'Moras uzeti od ponuđenih odabira' .PHP_EOL;
+            continue;
+        }
+        break;
+
+    }  switch($izbor){
+        case 1:
+            $this->dodajuKosaricu();
+            break;
+       case 2:
+            $this->izbrisiIzKosarice();
+            break;
+         case 3:
+            $this->racun();
+            break;
+            case 4:
+           echo 'Doviđenja' ;
+            
+        } 
+    }
+
+
+        private function dodajuKosaricu()
+        {
+            $produktKosarice=new Produkt();
+
+            for($i=0;$i<count($this->podaci);$i++){
+                echo $this->podaci[$i]->getSifra() . ' ' . $this->podaci[$i]->getIme()
+                . ' ' . $this->podaci[$i]->getKolicina() . ' ' . $this->podaci[$i]->getCijena() . PHP_EOL;
+                
+
+
+
+
+        }
+
+        while (true) {
+            $unos =(Controller::ucitajInt('Upiši sifru produkta: '));
+            foreach($this->podaci as $p){
+                if($unos ==$p->getSifra($unos)){
+                $brojProdukta->setSifra($unos);
+                }
+            }
+        
+        if(null !==($brojProdukta->getSifra())){
+            break;
+        }
+
+        echo 'šifra tog predmeta ne postoji' . PHP_EOL;
+        }
+
+        
+    
            
     
-   
+    }
 
 
 
